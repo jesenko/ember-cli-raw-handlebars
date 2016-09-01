641bd7758df58e83c034832391d448e42b8d2ff2
Asher Cohen <asherlc@asherlc.com>
2016-09-01 13:18:51 -0700

    add caching strategy

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ module.exports = {
   init: function() {
     checker.assertAbove(this, '2.4.3');
   },
+  baseDir: function() {
+    return __dirname;
+  },
   included: function(app) {
   },
   projectConfig: function() {
