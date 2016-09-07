9ceccd651438d27eb2f13531f31c17690be8a7a8
Asher Cohen <asherlc@asherlc.com>
2016-09-07 11:21:18 -0700

    fix deprecation

--- app/initializers/ember-cli-raw-handlebars.js
+++ app/initializers/ember-cli-raw-handlebars.js
@@ -1,14 +1,15 @@
 import Ember from 'ember';
 import Handlebars from 'npm:handlebars';
 
-var initialize = function initialize(container, application) {
-  let mainContainer = container;
+var initialize = function initialize(appInstance) {
+  let mainContainer = appInstance.container;
   let emberGetHelper = function(args, options) {
     if (args) {
       return Ember.get(this, args);
     }
   };
-  let componentHelper = function(args,options) {
+
+  let componentHelper = (args,options) {
     let container = this.container || mainContainer;
     let componentName = args;
     let component = container.lookup('component:' + componentName);
@@ -18,6 +19,7 @@ var initialize = function initialize(container, application) {
     component.render(buffer);
     return new Handlebars.SafeString(buffer[0]);
   };
+
   let nonblockIfHelper = function(args, options) {
     if (args[0]){
       return args[1];
@@ -25,6 +27,7 @@ var initialize = function initialize(container, application) {
       return args[2];
     }
   };
+
   Handlebars.registerHelper('get', emberGetHelper);
   Handlebars.registerHelper('component', componentHelper);
   Handlebars.registerHelper('iff', nonblockIfHelper);
